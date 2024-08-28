572e0d6caf8b6a81215a55cce4cf730cdc14e85e
NewBornRustacean <huiseomkim@gmail.com>
2024-08-28 09:20:27 +0900

    async refactor

--- src/chat_stream.rs
+++ src/chat_stream.rs
@@ -1,36 +1,28 @@
-use async_openai::types::{ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs};
-use async_openai::Client as OpenaiClient;
+use async_openai::{
+    config::OpenAIConfig,
+    types::{ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs},
+    Client as OpenaiClient,
+};
 use axum::{
     extract::{Path, Query, State},
-    response::sse::Event,
-    response::{IntoResponse, Sse},
+    response::{IntoResponse, Response},
 };
-use futures::{FutureExt, StreamExt};
-use mongodb::bson::uuid;
-use mongodb::{Client as MongoClient, Collection};
-use serde::{Deserialize, Serialize};
-use std::error::Error;
-use std::sync::Arc;
-use tokio::sync::{mpsc, Mutex};
-use tokio_stream::wrappers::ReceiverStream;
+use futures::StreamExt;
+use std:: sync::Arc;
+use utoipa::{IntoParams, ToSchema};
 use uuid::Uuid;
-use utoipa::{ToSchema, IntoParams};
+use anyhow::{Result, Error};
 
 #[derive(serde::Deserialize, ToSchema, IntoParams)]
 pub struct ChatParams {
     prompt: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, ToSchema, IntoParams)]
-pub struct ChatRecord {
-    userid: Uuid,
-    chatid: Uuid,
-    prompt: String,
-    response: String,
+
+pub struct AppState {
+    pub openai_client: OpenaiClient<OpenAIConfig>,
+    // pub records: Arc<Mutex<RecordStore>>,
 }
-
-type ChatQueue = Arc<Mutex<mpsc::Sender<ChatRecord>>>;
-
 
 #[utoipa::path(
     get,
@@ -48,87 +40,38 @@ type ChatQueue = Arc<Mutex<mpsc::Sender<ChatRecord>>>;
 pub async fn chat_stream_handler(
     Path((userid, chatid)): Path<(Uuid, Uuid)>,
     Query(params): Query<ChatParams>,
-    State(chat_queue): State<ChatQueue>,
+    State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
-    let (tx, rx) = mpsc::channel(100);
-    let chat_queue_clone = Arc::clone(&chat_queue);
-
-    tokio::spawn(async move {
-        if let Err(e) = generate_chat_stream(params.prompt.as_str(), tx, chat_queue_clone, userid, chatid).await {
-            println!("failed to stream chat: {}", e);
-        }
-    });
-
-    let stream = ReceiverStream::new(rx);
-    Sse::new(stream.map(|msg| Ok::<_, std::convert::Infallible>(Event::default().data(msg))))
-}
-
-pub async fn generate_chat_stream(
-    prompt: &str,
-    tx: mpsc::Sender<String>,
-    chat_queue: ChatQueue,
-    userid: Uuid,
-    chatid: Uuid,
-) -> Result<(), Box<dyn Error>> {
-    let openai_client = OpenaiClient::new();
-    let mut accumulated_content = String::new();
-
     let request = CreateChatCompletionRequestArgs::default()
         .model("gpt-4o-mini")
         .max_tokens(512u32)
         .messages([ChatCompletionRequestUserMessageArgs::default()
-            .content(&*prompt)
-            .build()?
+            .content(&*params.prompt)
+            .build()
+            .unwrap()
             .into()])
-        .build()?;
+        .build()
+        .unwrap();
 
-    let mut stream = openai_client.chat().create_stream(request).await?;
+    let mut stream = state.openai_client.chat().create_stream(request).await.unwrap();
 
-    while let Some(result) = stream.next().await {
-        match result {
-            Ok(response) => {
-                for chat_choice in response.choices.iter() {
-                    if let Some(ref content) = chat_choice.delta.content {
-                        accumulated_content.push_str(content);
-
-                        if let Err(e) = tx.send(content.to_string()).await {
-                            eprintln!("failed to send content: {}", e);
+    let body = axum::body::Body::from_stream(async_stream::stream! {
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(response) => {
+                    for chat_choice in response.choices {
+                        if let Some(content) = chat_choice.delta.content {
+                            yield Ok::<std::string::String, Error>(content);
                         }
                     }
                 }
-            }
-            Err(err) => {
-                if let Err(e) = tx.send(format!("error: {err}")).await {
-                    eprintln!("failed to send error message: {}", e);
+                Err(err) => {
+                    eprintln!("Error in streaming response: {}", err);
                 }
             }
         }
-    }
+    });
 
-    let chat_record = ChatRecord {
-        userid,
-        chatid,
-        prompt: prompt.to_string(),
-        response: accumulated_content.clone(),
-    };
-
-    if let Err(e) = chat_queue.lock().await.send(chat_record).await {
-        eprintln!("failed to push job to queue: {}", e);
-    }
-
-
-    Ok(())
+    Response::new(body)
 }
 
-pub async fn insert_to_db_from_queue( mut rx: mpsc::Receiver<ChatRecord>) -> Result<(), Box<dyn Error>> {
-    // let collection: Collection<ChatRecord> = db_client.database("chat_db").collection("chat_records");
-
-    while let Some(job) = rx.recv().await {
-        println!("consumed: {:?}", job);
-        // // Insert the job (input-output pair) into MongoDB
-        // if let Err(e) = collection.insert_one(job).await {
-        //     eprintln!("failed to insert job into MongoDB: {}", e);
-        // }
-    }
-    Ok(())
-}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,44 @@
 mod chat_stream;
-use chat_stream::{chat_stream_handler, insert_to_db_from_queue,ChatParams, ChatRecord, __path_chat_stream_handler};
+
+use async_openai::Client as OpenAIClient;
 use axum::{response::IntoResponse, routing::get, Router};
+use chat_stream::{
+    chat_stream_handler, AppState, ChatParams, __path_chat_stream_handler,
+};
 use futures::StreamExt;
 use mongodb::{Client as MongoClient, Collection};
+use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
 use tokio::sync::{mpsc, Mutex};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
+
 #[derive(OpenApi)]
-#[openapi(paths(chat_stream_handler), components(schemas(ChatParams, ChatRecord)))]
+#[openapi(
+    paths(chat_stream_handler),
+    components(schemas(ChatParams))
+)]
 struct ApiDoc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let (tx, rx) = mpsc::channel::<ChatRecord>(100);
-    let chat_queue = Arc::new(Mutex::new(tx));
-    // let db_client = MongoClient::with_uri_str(":memory:").await?;
-
-    // Start the job consumer in a background task
-    tokio::spawn(async move {
-        if let Err(e) = insert_to_db_from_queue(rx).await {
-            eprintln!("Failed to consume job queue: {}", e);
-        }
+    // let openai_config
+    let openai_client = OpenAIClient::new();
+    let app_state = Arc::new(AppState {
+        openai_client: openai_client,
     });
 
     let router = Router::new()
         .route("/chat-stream/:userid/:chatid", get(chat_stream_handler))
-        .with_state(chat_queue)
+        .with_state(app_state)
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()));
-
 
     let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
 
+    println!("Server running on http://127.0.0.1:3000");
+
     axum::serve(listener, router).await.unwrap();
+
     Ok(())
 }
-
