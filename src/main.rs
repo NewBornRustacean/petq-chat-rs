22764f3377b798b64e8b58a05831b2524e3f96bb
NewBornRustacean <huiseomkim@gmail.com>
2024-09-07 22:27:49 +0900

    prompt format, db simulation: hashmap added.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod memory;
 
 use async_openai::Client as OpenaiClient;
 use axum::{response::IntoResponse, routing::get, Router};
-use chat_stream::{chat_stream_handler, ChatRecord, ChatParams, __path_chat_stream_handler};
+use chat_stream::{chat_stream_handler, ChatRecord, ChatParams, PetProfileDB, PetProfile, SharedState, __path_chat_stream_handler};
 use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
@@ -13,6 +13,8 @@ use utoipa_swagger_ui::SwaggerUi;
 use tower_http::cors::{CorsLayer, Any};
 use serde::Deserialize;
 use std::fs;
+use uuid::{uuid, Uuid};
+
 #[derive(OpenApi)]
 #[openapi(paths(chat_stream_handler), components(schemas(ChatParams, ChatRecord)))]
 struct ApiDoc;
@@ -36,9 +38,13 @@ impl AppConfig {
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 16)]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let chat_collection: Arc<Mutex<HashMap<String, ChatRecord>>> = Arc::new(Mutex::new(HashMap::new()));
     let chat_stream_config = AppConfig::from_file("src/chat_stream_config.toml")?;
-    let openai_client = Arc::new(OpenaiClient::new());
+
+    let shared_state = SharedState {
+        chat_collection: Arc::new(Mutex::new(HashMap::new())),
+        openai_client: Arc::new(OpenaiClient::new()), // Initialize your OpenAI client here
+        pet_profile_db: Arc::new(Mutex::new(HashMap::new())), // Your pet profile "DB"
+    };
 
     // Configure CORS
     let cors_layer = CorsLayer::new()
@@ -49,10 +55,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // Allow sending any headers
         .allow_headers(Any);
 
+    {
+        let mut db = shared_state.pet_profile_db.lock().await;
+        db.insert(
+            uuid!("67e55044-10b1-426f-9247-bb680e5fe0c8"),
+            PetProfile {
+                name: "설렘".to_string(),
+                species: "강아지, 포메라니안".to_string(),
+                age: "1".to_string(),
+                health_concerns: "중성화 수술 완료. 닭고기 알러지 있음.".to_string(),
+            },
+        );
+    }
+
     // Set up the Axum router with shared HashMap and OpenAI client
     let router = Router::new()
         .route("/chat-stream/:userid/:chatid", get(chat_stream_handler))
-        .with_state((chat_collection, openai_client))
+        .with_state(shared_state)
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .layer(cors_layer);
 
